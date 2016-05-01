f232ad40578f021f5dc2759236875cd37a4f830e
jerry1162 <1162328204@qq.com>
2016-05-01 12:03:08 +0800

    成功搭建基本框架

--- DUI_Demo/DUI_Window.cpp
+++ DUI_Demo/DUI_Window.cpp
@@ -1,23 +1,42 @@
 #include "stdafx.h"
 #include "DUI_Window.h"
 
-//LRESULT DUI_Window::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-//{
-//	return LRESULT();
-//}
-static LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+
+
+DUI_Window::DUI_Window()
 {
-	return 1;
+	static DUI_Window* m_pCurObj = this;
+	static LONG PrevWndProc = NULL;
 }
+
 DUI_Window::DUI_Window(HWND hWnd)
 {
-
+	static DUI_Window* m_pCurObj =this;
+	static LONG PrevWndProc = NULL;
+	Create(hWnd);
 }
 
 
 DUI_Window::~DUI_Window()
 {
 
+}
+
+LRESULT DUI_Window::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static DUI_Window* m_pCurObj;
+	static LONG PrevWndProc;
+
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		MessageBeep(MB_ICONINFORMATION);
+		break;
+	default:
+		break;
+	}
+
+	return CallWindowProc(WNDPROC(PrevWndProc),hwnd,uMsg,wParam,lParam);
 }
 
 BOOL DUI_Window::Create(HWND hWnd)
@@ -30,12 +49,13 @@ BOOL DUI_Window::Create(HWND hWnd)
 	m_WndRect.Y = rect.top;
 	m_WndRect.Width = rect.right - rect.left;
 	m_WndRect.Height = rect.bottom - rect.top;
-	m_MemGraphics.Create(m_WndRect.Width, m_WndRect.Height);
+	m_MemDC->Create(m_WndRect.Width, m_WndRect.Height);
 	LPWSTR str= L"";
 	GetWindowText(hWnd, str, 256);
 	m_Caption->string = str;
 	m_Caption->format.GenericDefault();
-
+	static LONG PrevWndProc;
+	PrevWndProc = SetWindowLong(hWnd, GWL_WNDPROC, LONG(DUI_Window::WndProc));
 	m_hWnd = hWnd;
 	return 0;
 }
--- DUI_Demo/DUI_Window.h
+++ DUI_Demo/DUI_Window.h
@@ -9,18 +9,22 @@ struct GdipString
 	StringFormat format;
 };
 
+
 class DUI_Window
 {
 public:
+	DUI_Window();
 	DUI_Window(HWND hWnd);
 	~DUI_Window();
-	static LRESULT WndProc;
 	BOOL Create(HWND hWnd);
 	BOOL Destroy();
 private:
+	static LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	HWND m_hWnd;
-	MemDC m_MemDC;
+	MemDC* m_MemDC;
 	Gdiplus::Graphics* m_Graphics;
 	Gdiplus::RectF m_WndRect;
 	GdipString* m_Caption;
+	static DUI_Window* m_pCurObj;
+	static LONG PrevWndProc;
 };
